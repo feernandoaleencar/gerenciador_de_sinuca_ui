ba1af5415b0b7c941b1823f9cd8af9012e20d2ed
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-08 22:11:11 -0300

    EU_01: criando componente cliente-listar

---
+++ src/app/feature/cliente/cliente-listar/cliente-listar.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-cliente-listar',
+    templateUrl: './cliente-listar.component.html',
+    styleUrls: ['./cliente-listar.component.scss']
+})
+export class ClienteListarComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+    inserir() {
+
+    }
+
+    editarCliente(product: any) {
+
+    }
+
+    deletarCliente(product: any) {
+
+    }
+}
