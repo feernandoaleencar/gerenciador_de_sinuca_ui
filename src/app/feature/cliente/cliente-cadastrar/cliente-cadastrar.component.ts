2aef6f73279fdd745873304f6b63f5cec37d866b
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-08 22:20:16 -0300

    EU_01: Criando componente cliente-cadastrar

---
+++ src/app/feature/cliente/cliente-cadastrar/cliente-cadastrar.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-cliente-cadastrar',
+    templateUrl: './cliente-cadastrar.component.html',
+    styleUrls: ['./cliente-cadastrar.component.scss']
+})
+export class ClienteCadastrarComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+
+    }
+
+    inserir() {
+
+    }
+
+}
