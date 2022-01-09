4b7d0a0dbe604bf7a7eb233cb43953a2adc81afd
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-08 21:08:57 -0300

    EU_01: table lista de cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { InputTextModule } from 'primeng/inputtext';
-import {CardModule} from "primeng/card";
-import {ToolbarModule} from "primeng/toolbar";
-import {ButtonModule} from "primeng/button";
-import {RippleModule} from "primeng/ripple";
+import { CardModule } from "primeng/card";
+import { ToolbarModule } from "primeng/toolbar";
+import { ButtonModule } from "primeng/button";
+import { RippleModule } from "primeng/ripple";
+import { TableModule } from "primeng/table";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    InputTextModule,
-    CardModule,
-    ToolbarModule,
-    ButtonModule,
-    RippleModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        InputTextModule,
+        CardModule,
+        ToolbarModule,
+        ButtonModule,
+        RippleModule,
+        TableModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
