45dbbc661e1a168b375a3267b654447c2b2c48e1
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-12 22:25:59 -0300

    EU_02: chamando modal e criando lista para sinuca-listar-component

--- src/app/feature/sinuca/sinuca-listar/sinuca-listar.component.ts
+++ src/app/feature/sinuca/sinuca-listar/sinuca-listar.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-sinuca-listar',
-  templateUrl: './sinuca-listar.component.html',
-  styleUrls: ['./sinuca-listar.component.scss']
+    selector: 'app-sinuca-listar',
+    templateUrl: './sinuca-listar.component.html',
+    styleUrls: ['./sinuca-listar.component.scss']
 })
 export class SinucaListarComponent implements OnInit {
 
-  constructor() { }
+    sinuca = [{
+        "cpf": "1000",
+        "telefone": "f230fh0g3",
+        "nome": "Bamboo Watch",
+        "description": "Product Description",
+        "image": "bamboo-watch.jpg",
+        "price": 65,
+        "cidade": "Accessories",
+        "quantity": 24,
+        "inventoryStatus": "INSTOCK",
+        "rating": 5
+    },
+        {
+            "cpf": "1001",
+            "telefone": "nvklal433",
+            "nome": "Black Watch",
+            "description": "Product Description",
+            "image": "black-watch.jpg",
+            "price": 72,
+            "cidade": "Accessories",
+            "quantity": 61,
+            "inventoryStatus": "INSTOCK",
+            "rating": 4
+        },
+        {
+            "cpf": "1002",
+            "telefone": "zz21cz3c1",
+            "nome": "Blue Band",
+            "description": "Product Description",
+            "image": "blue-band.jpg",
+            "price": 79,
+            "cidade": "Fitness",
+            "quantity": 2,
+            "inventoryStatus": "LOWSTOCK",
+            "rating": 3
+        }]
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
 
+    ngOnInit(): void {
+    }
+
+    editarSinuca(sinuca: any) {
+
+    }
+
+    deletarSinuca(sinuca: any){
+    }
 }
