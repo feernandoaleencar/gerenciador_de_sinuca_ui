8fe028e57bbc95b7cdbec74eb80c45980d1bf933
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-12 22:17:12 -0300

    EU_02: criando sinuca-listar-component

---
+++ src/app/feature/sinuca/sinuca-listar/sinuca-listar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sinuca-listar',
+  templateUrl: './sinuca-listar.component.html',
+  styleUrls: ['./sinuca-listar.component.scss']
+})
+export class SinucaListarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
