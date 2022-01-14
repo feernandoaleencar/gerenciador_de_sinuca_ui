48e0f6278c53a4786d05b52a5871d968ce44f863
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-13 23:55:39 -0300

    EU_02: Inserir sinuca-movimentar

--- src/app/feature/sinuca/sinuca-movimentar/sinuca-movimentar.component.ts
+++ src/app/feature/sinuca/sinuca-movimentar/sinuca-movimentar.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-sinuca-movimentar',
-  templateUrl: './sinuca-movimentar.component.html',
-  styleUrls: ['./sinuca-movimentar.component.scss']
+    selector: 'app-sinuca-movimentar',
+    templateUrl: './sinuca-movimentar.component.html',
+    styleUrls: ['./sinuca-movimentar.component.scss']
 })
 export class SinucaMovimentarComponent implements OnInit {
+    sinucaDialog: boolean = false;
+    submitted: boolean = true;
 
-  constructor() { }
+    cliente: any;
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
 
+    ngOnInit(): void {
+    }
+
+    inserir() {
+        this.cliente = {};
+        this.submitted = false;
+        this.sinucaDialog = true;
+    }
 }
