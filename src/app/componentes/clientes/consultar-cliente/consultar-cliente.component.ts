bb63eab048389ee554a1f6e1480e9d923cc21c0e
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-09-27 22:31:53 -0300

    Ajuste css e inserindo botão para cadastrar novo cliente

--- src/app/componentes/clientes/consultar-cliente/consultar-cliente.component.ts
+++ src/app/componentes/clientes/consultar-cliente/consultar-cliente.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-consultar-cliente',
-  templateUrl: './consultar-cliente.component.html',
-  styleUrls: ['./consultar-cliente.component.scss']
+    selector: 'app-consultar-cliente',
+    templateUrl: './consultar-cliente.component.html',
+    styleUrls: ['./consultar-cliente.component.scss']
 })
 export class ConsultarClienteComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        private router: Router,
+    ) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    irParaPagina() {
+        this.router.navigate(['/clientes/novo'])
+    }
 
 }
