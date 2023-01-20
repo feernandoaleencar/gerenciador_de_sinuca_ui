b3fce962fb1e07ae2ec6e83ff7b591a781c18c75
Fernando Alencar <feernando_aleencar@hotmail.com>
2023-01-19 23:18:35 -0300

    Incluindo componente de Toast

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,9 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NavbarComponent} from "./navbar/navbar.component";
 import {RouterModule} from "@angular/router";
+import {ToastModule} from "primeng/toast";
+import {ConfirmationService, MessageService} from "primeng/api";
+import {ConfirmDialogModule} from "primeng/confirmdialog";
 
 
 @NgModule({
@@ -12,10 +15,19 @@ import {RouterModule} from "@angular/router";
         CommonModule,
         RouterModule,
 
+        ToastModule,
+        ConfirmDialogModule,
     ],
     exports: [
         NavbarComponent,
-    ]
+
+        ToastModule,
+        ConfirmDialogModule,
+    ],
+    providers: [
+        MessageService,
+        ConfirmationService
+    ],
 })
 export class CoreModule {
 }
